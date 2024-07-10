0462d74b4cb285f431de2982da8265efed82da31
harikrishnaaa321 <bharikrishna64@gmail.com>
2024-07-10 17:52:58 +0530

    firebase signup completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,7 +15,9 @@ const App = () => {
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/about' element={<About/>}/>
+      <Route element={<PrivateRoute/>}>
       <Route path='/profile' element={<Profile/>}/>
+      </Route>
 
     </Routes>
     </BrowserRouter>
